e7d5a5b322d36eebe240cde3a7b09997d6826bc8
Mike Redrobe <mike@redrobe.net>
2015-10-30 16:53:09 +0000

    Connect via I2c, light a row up
    Connect via I2c, light a row up

---
+++ test.lua
@@ -0,0 +1,14 @@
+id=0
+sda=1
+scl=2
+CODEBUG=0x18
+SET=3
+LED_LINE=1
+
+i2c.setup(id,sda,scl,i2c.SLOW)
+i2c.start(id)
+i2c.address(id,CODEBUG,i2c.TRANSMITTER)
+i2c.write(id,SET)
+i2c.write(id,LED_LINE)
+i2c.write(id,15)
+i2c.stop
