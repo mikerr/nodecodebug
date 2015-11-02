271a32a96de78aee03a244fa483967e942cbe440
Mike Redrobe <mike@redrobe.net>
2015-11-02 16:37:49 +0000

    Update codebug.lua

--- codebug.lua
+++ codebug.lua
@@ -63,7 +63,7 @@ end
 
 function codebug_scroll_string(message,delay)
   for x = 1, string.len(message)-1 do
-  	for scroll=1,8 do
+  	for scroll=1,7 do
 		for i=1,5 do
 			char1 = font[string.sub(message,x,x)][i] --
 			char2 = font[string.sub(message,x+1,x+1)][i] --
